3dd65bfa76930a67ef4e3206d3b34976dc91dcfe
AKruimink <43521957+AKruimink@users.noreply.github.com>
2025-01-26 03:07:43 +0000

    Add-projects

---
+++ src/MapMarkers/MapMarkersModule.cs
@@ -0,0 +1,12 @@
+﻿using Blish_HUD.Modules;
+using System.ComponentModel.Composition;
+
+namespace BHM_MapMarkers;
+
+[Export(typeof(Module))]
+public class MapMarkersModule : Module
+{
+    public MapMarkersModule([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters)
+    {
+    }
+}
